e3b48ce404484d0a67994b686e8fe043a53d8b45
moloko <mleathes+github@gmail.com>
2018-11-14 15:21:51 +0000

    switch over to the new ComponentView inview functionality

--- js/assessmentResultsView.js
+++ js/assessmentResultsView.js
@@ -6,7 +6,6 @@ define([
     var AssessmentResultsView = ComponentView.extend({
 
         events: {
-            'inview': 'onInview',
             'click .results-retry-button button': 'onRetryClicked'
         },
 
@@ -27,29 +26,7 @@ define([
 
         postRender: function() {
             this.setReadyStatus();
-        },
-
-        onInview: function(event, visible, visiblePartX, visiblePartY) {
-            if (!visible) return;
-
-            if (visiblePartY === 'top') {
-                this._isVisibleTop = true;
-            } else if (visiblePartY === 'bottom') {
-                this._isVisibleBottom = true;
-            } else {
-                this._isVisibleTop = true;
-                this._isVisibleBottom = true;
-            }
-
-            if (this._isVisibleTop || this._isVisibleBottom) {
-                this.model.checkCompletion();
-                // Sometimes (with mobile and virtual keyboards) inview can be triggered
-                // but the component is not _visible = true, so it does not get marked
-                // complete. Delay the unbinding of the inview listener until complete
-                if (this.model.get('_isComplete')) {
-                    this.$el.off('inview');
-                }
-            }
+            this.setupInviewListener('.component-inner', this.model.checkCompletion.bind(this.model));
         },
 
         onRetryClicked: function() {

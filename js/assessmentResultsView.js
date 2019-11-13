87aa2a7914470dc81aa433eb83395e62115de399
Guy Willis <guywillis@users.noreply.github.com>
2019-11-13 16:10:24 +0000

    Release/v5 (#54)
    * Converted tabs to spaces, indentation amends
    
    * Naming refactor
    
    * Removed empty less file
    
    * Button class update
    
    * Removed is-generic class
    
    * Removed less
    
    * Bower, example and readme updates
    
    * Updated license and cross platform coverage

--- js/assessmentResultsView.js
+++ js/assessmentResultsView.js
@@ -1,60 +1,61 @@
 define([
-    'core/js/adapt',
-    'core/js/views/componentView'
+  'core/js/adapt',
+  'core/js/views/componentView'
 ], function(Adapt, ComponentView) {
 
-    var AssessmentResultsView = ComponentView.extend({
+  var AssessmentResultsView = ComponentView.extend({
 
-        events: {
-            'click .results-retry-button button': 'onRetryClicked'
-        },
+    events: {
+      'click .js-assessment-retry-btn': 'onRetryClicked'
+    },
 
-        preRender: function () {
-            this.model.setLocking('_isVisible', false);
+    preRender: function () {
+      this.model.setLocking('_isVisible', false);
 
-            this.listenTo(Adapt, 'preRemove', function () {
-                this.model.unsetLocking('_isVisible');
-            });
+      this.listenTo(Adapt, 'preRemove', function () {
+        this.model.unsetLocking('_isVisible');
+      });
 
-            this.listenTo(this.model, {
-                'change:_feedbackBand': this.addClassesToArticle,
-                'change:body': this.render
-            });
-        },
+      this.listenTo(this.model, {
+        'change:_feedbackBand': this.addClassesToArticle,
+        'change:body': this.render
+      });
+    },
 
-        postRender: function() {
-            this.model.checkIfAssessmentComplete();
-            this.setReadyStatus();
-            this.setupInviewCompletion('.component-inner', this.model.checkCompletion.bind(this.model));
-        },
+    postRender: function() {
+      this.model.checkIfAssessmentComplete();
+      this.setReadyStatus();
+      this.setupInviewCompletion('.component__inner', this.model.checkCompletion.bind(this.model));
+    },
 
-        /**
-         * Resets the state of the assessment and optionally redirects the user
-         * back to the assessment for another attempt.
-         */
-        onRetryClicked: function() {
-            var state = this.model.get('_state');
+    /**
+     * Resets the state of the assessment and optionally redirects the user
+     * back to the assessment for another attempt.
+     */
+    onRetryClicked: function() {
+      var state = this.model.get('_state');
 
-            Adapt.assessment.get(state.id).reset();
+      Adapt.assessment.get(state.id).reset();
 
-            if (this.model.get('_retry')._routeToAssessment === true) {
-                Adapt.navigateToElement('.' + state.articleId);
-            }
-        },
+      if (this.model.get('_retry')._routeToAssessment === true) {
+        Adapt.navigateToElement('.' + state.articleId);
+      }
+    },
 
-        /**
-         * If there are classes specified for the feedback band, apply them to the containing article
-         * This allows for custom styling based on the band the user's score falls into
-         */
-        addClassesToArticle: function(model, value) {
-            if (!value || !value._classes) return;
+    /**
+     * If there are classes specified for the feedback band, apply them to the containing article
+     * This allows for custom styling based on the band the user's score falls into
+     */
+    addClassesToArticle: function(model, value) {
+      if (!value || !value._classes) return;
 
-            this.$el.parents('.article').addClass(value._classes);
-        }
+      this.$el.parents('.article').addClass(value._classes);
+    }
 
-    }, {
-        template: 'assessmentResults'
-    });
+  }, {
+    template: 'assessmentResults'
+  });
 
-    return AssessmentResultsView;
+  return AssessmentResultsView;
+
 });

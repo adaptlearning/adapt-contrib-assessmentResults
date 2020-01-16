f6206198b020a44e5a2d9d1ed7f8316a53fca477
Guy Willis <guywillis@users.noreply.github.com>
2020-01-16 11:03:52 +0000

    Added state variable back in

--- js/assessmentResultsModel.js
+++ js/assessmentResultsModel.js
@@ -25,6 +25,7 @@ define([
       var assessmentModel = Adapt.assessment.get(this.get('_assessmentId'));
       if (!assessmentModel || assessmentModel.length === 0) return;
 
+      var state = assessmentModel.getState();
       var isResetOnRevisit = assessmentModel.get('_assessment')._isResetOnRevisit;
       if (state.isComplete && (!state.allowResetIfPassed || !isResetOnRevisit)) {
         this.onAssessmentComplete(state);

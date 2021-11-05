125e63b1a6df0bc16998d5c0c19df36f8ca91df2
Eleanor Heath <eleanor.heath@learningpoool.com>
2021-11-05 15:24:02 +0000

    2808 - convert to es6 classes

--- js/assessmentResultsModel.js
+++ js/assessmentResultsModel.js
@@ -1,170 +1,166 @@
-define([
-  'core/js/adapt',
-  'core/js/models/componentModel'
-], function(Adapt, ComponentModel) {
+import Adapt from 'core/js/adapt';
+import ComponentModel from 'core/js/models/componentModel';
 
-  class AssessmentResultsModel extends ComponentModel {
+export default class AssessmentResultsModel extends ComponentModel {
 
-    init(...args) {
-      this.set('originalBody', this.get('body'));// save the original body text so we can restore it when the assessment is reset
+  init(...args) {
+    // save the original body text so we can restore it when the assessment is reset
+    this.set('originalBody', this.get('body'));
 
-      this.listenTo(Adapt, {
-        'assessments:complete': this.onAssessmentComplete,
-        'assessments:reset': this.onAssessmentReset
-      });
+    this.listenTo(Adapt, {
+      'assessments:complete': this.onAssessmentComplete,
+      'assessments:reset': this.onAssessmentReset
+    });
 
-      super.init(...args);
+    super.init(...args);
+  }
+
+  /**
+   * Checks to see if the assessment was completed in a previous session or not
+   */
+  checkIfAssessmentComplete() {
+    if (!Adapt.assessment || this.get('_assessmentId') === undefined) {
+      return;
     }
 
-    /**
-     * Checks to see if the assessment was completed in a previous session or not
-     */
-    checkIfAssessmentComplete() {
-      if (!Adapt.assessment || this.get('_assessmentId') === undefined) {
-        return;
-      }
+    const assessmentModel = Adapt.assessment.get(this.get('_assessmentId'));
+    if (!assessmentModel || assessmentModel.length === 0) return;
 
-      const assessmentModel = Adapt.assessment.get(this.get('_assessmentId'));
-      if (!assessmentModel || assessmentModel.length === 0) return;
-
-      const state = assessmentModel.getState();
-      const isResetOnRevisit = assessmentModel.get('_assessment')._isResetOnRevisit;
-      if (state.isComplete && (!state.allowResetIfPassed || !isResetOnRevisit)) {
-        this.onAssessmentComplete(state);
-        return;
-      }
-
-      this.setVisibility();
+    const state = assessmentModel.getState();
+    const isResetOnRevisit = assessmentModel.get('_assessment')._isResetOnRevisit;
+    if (state.isComplete && (!state.allowResetIfPassed || !isResetOnRevisit)) {
+      this.onAssessmentComplete(state);
+      return;
     }
 
-    onAssessmentComplete(state) {
-      if (this.get('_assessmentId') === undefined ||
-          this.get('_assessmentId') !== state.id) return;
+    this.setVisibility();
+  }
 
-      /*
-      make shortcuts to some of the key properties in the state object so that
-      content developers can just use {{attemptsLeft}} in json instead of {{state.attemptsLeft}}
-      */
-      this.set({
-        _state: state,
-        attempts: state.attempts,
-        attemptsSpent: state.attemptsSpent,
-        attemptsLeft: state.attemptsLeft,
-        score: state.score,
-        scoreAsPercent: state.scoreAsPercent,
-        maxScore: state.maxScore,
-        isPass: state.isPass
-      });
+  onAssessmentComplete(state) {
+    if (this.get('_assessmentId') === undefined ||
+        this.get('_assessmentId') !== state.id) return;
 
-      this.setFeedbackBand(state);
+    /*
+    make shortcuts to some of the key properties in the state object so that
+    content developers can just use {{attemptsLeft}} in json instead of {{state.attemptsLeft}}
+    */
+    this.set({
+      _state: state,
+      attempts: state.attempts,
+      attemptsSpent: state.attemptsSpent,
+      attemptsLeft: state.attemptsLeft,
+      score: state.score,
+      scoreAsPercent: state.scoreAsPercent,
+      maxScore: state.maxScore,
+      isPass: state.isPass
+    });
 
-      this.checkRetryEnabled(state);
+    this.setFeedbackBand(state);
 
-      this.setFeedbackText();
+    this.checkRetryEnabled(state);
 
-      this.toggleVisibility(true);
-    }
+    this.setFeedbackText();
 
-    setFeedbackBand(state) {
-      const scoreProp = state.isPercentageBased ? 'scoreAsPercent' : 'score';
-      const bands = _.sortBy(this.get('_bands'), '_score');
+    this.toggleVisibility(true);
+  }
 
-      for (let i = (bands.length - 1); i >= 0; i--) {
-        const isScoreInBandRange = (state[scoreProp] >= bands[i]._score);
-        if (!isScoreInBandRange) continue;
+  setFeedbackBand(state) {
+    const scoreProp = state.isPercentageBased ? 'scoreAsPercent' : 'score';
+    const bands = _.sortBy(this.get('_bands'), '_score');
 
-        this.set('_feedbackBand', bands[i]);
-        break;
-      }
-    }
+    for (let i = (bands.length - 1); i >= 0; i--) {
+      const isScoreInBandRange = (state[scoreProp] >= bands[i]._score);
+      if (!isScoreInBandRange) continue;
 
-    checkRetryEnabled(state) {
-      const assessmentModel = Adapt.assessment.get(state.id);
-      if (!assessmentModel.canResetInPage()) return false;
-
-      const feedbackBand = this.get('_feedbackBand');
-      const isRetryEnabled = (feedbackBand && feedbackBand._allowRetry) !== false;
-      const isAttemptsLeft = (state.attemptsLeft > 0 || state.attemptsLeft === 'infinite');
-      const showRetry = isRetryEnabled && isAttemptsLeft && (!state.isPass || state.allowResetIfPassed);
-
-      this.set({
-        _isRetryEnabled: showRetry,
-        retryFeedback: showRetry ? this.get('_retry').feedback : ''
-      });
-    }
-
-    setFeedbackText() {
-      const feedbackBand = this.get('_feedbackBand');
-
-      // ensure any handlebars expressions in the .feedback are handled...
-      const feedback = feedbackBand ? Handlebars.compile(feedbackBand.feedback)(this.toJSON()) : '';
-
-      this.set({
-        feedback,
-        body: this.get('_completionBody')
-      });
-    }
-
-    setVisibility() {
-      if (!Adapt.assessment) return;
-
-      const assessmentModel = Adapt.assessment.get(this.get('_assessmentId'));
-      if (!assessmentModel || assessmentModel.length === 0) return;
-
-      const state = assessmentModel.getState();
-      const isAttemptInProgress = state.attemptInProgress;
-      const isComplete = !isAttemptInProgress && state.isComplete;
-      const isVisibleBeforeCompletion = this.get('_isVisibleBeforeCompletion') || false;
-      const isVisible = isVisibleBeforeCompletion || isComplete;
-
-      this.toggleVisibility(isVisible);
-    }
-
-    toggleVisibility (isVisible) {
-      if (isVisible === undefined) {
-        isVisible = !this.get('_isVisible');
-      }
-
-      this.set('_isVisible', isVisible, { pluginName: 'assessmentResults' });
-    }
-
-    checkCompletion() {
-      if (this.get('_setCompletionOn') === 'pass' && !this.get('isPass')) {
-        return;
-      }
-
-      this.setCompletionStatus();
-    }
-
-    /**
-     * Handles resetting the component whenever its corresponding assessment is reset
-     * The component can either inherit the assessment's reset type or define its own
-     */
-    onAssessmentReset(state) {
-      if (this.get('_assessmentId') === undefined ||
-          this.get('_assessmentId') !== state.id) return;
-
-      let resetType = this.get('_resetType');
-      if (!resetType || resetType === 'inherit') {
-        resetType = state.resetType || 'hard';// backwards compatibility - state.resetType was only added in assessment v2.3.0
-      }
-      this.reset(resetType, true);
-    }
-
-    reset(...args) {
-      this.set({
-        body: this.get('originalBody'),
-        state: null,
-        feedback: '',
-        _feedbackBand: null,
-        retryFeedback: '',
-        _isRetryEnabled: false
-      });
-
-      super.reset(...args);
+      this.set('_feedbackBand', bands[i]);
+      break;
     }
   }
 
-  return AssessmentResultsModel;
+  checkRetryEnabled(state) {
+    const assessmentModel = Adapt.assessment.get(state.id);
+    if (!assessmentModel.canResetInPage()) return false;
 
-});
+    const feedbackBand = this.get('_feedbackBand');
+    const isRetryEnabled = (feedbackBand && feedbackBand._allowRetry) !== false;
+    const isAttemptsLeft = (state.attemptsLeft > 0 || state.attemptsLeft === 'infinite');
+    const showRetry = isRetryEnabled && isAttemptsLeft && (!state.isPass || state.allowResetIfPassed);
+
+    this.set({
+      _isRetryEnabled: showRetry,
+      retryFeedback: showRetry ? this.get('_retry').feedback : ''
+    });
+  }
+
+  setFeedbackText() {
+    const feedbackBand = this.get('_feedbackBand');
+
+    // ensure any handlebars expressions in the .feedback are handled...
+    const feedback = feedbackBand ? Handlebars.compile(feedbackBand.feedback)(this.toJSON()) : '';
+
+    this.set({
+      feedback,
+      body: this.get('_completionBody')
+    });
+  }
+
+  setVisibility() {
+    if (!Adapt.assessment) return;
+
+    const assessmentModel = Adapt.assessment.get(this.get('_assessmentId'));
+    if (!assessmentModel || assessmentModel.length === 0) return;
+
+    const state = assessmentModel.getState();
+    const isAttemptInProgress = state.attemptInProgress;
+    const isComplete = !isAttemptInProgress && state.isComplete;
+    const isVisibleBeforeCompletion = this.get('_isVisibleBeforeCompletion') || false;
+    const isVisible = isVisibleBeforeCompletion || isComplete;
+
+    this.toggleVisibility(isVisible);
+  }
+
+  toggleVisibility(isVisible) {
+    if (isVisible === undefined) {
+      isVisible = !this.get('_isVisible');
+    }
+
+    this.set('_isVisible', isVisible, { pluginName: 'assessmentResults' });
+  }
+
+  checkCompletion() {
+    if (this.get('_setCompletionOn') === 'pass' && !this.get('isPass')) {
+      return;
+    }
+
+    this.setCompletionStatus();
+  }
+
+  /**
+   * Handles resetting the component whenever its corresponding assessment is reset
+   * The component can either inherit the assessment's reset type or define its own
+   */
+  onAssessmentReset(state) {
+    if (this.get('_assessmentId') === undefined ||
+        this.get('_assessmentId') !== state.id) return;
+
+    let resetType = this.get('_resetType');
+    if (!resetType || resetType === 'inherit') {
+      // backwards compatibility - state.resetType was only added in assessment v2.3.0
+      resetType = state.resetType || 'hard';
+    }
+    this.reset(resetType, true);
+  }
+
+  reset(...args) {
+    this.set({
+      body: this.get('originalBody'),
+      state: null,
+      feedback: '',
+      _feedbackBand: null,
+      retryFeedback: '',
+      _isRetryEnabled: false
+    });
+
+    super.reset(...args);
+  }
+}
--- js/assessmentResultsView.js
+++ js/assessmentResultsView.js
@@ -1,66 +1,61 @@
-define([
-  'core/js/adapt',
-  'core/js/views/componentView'
-], function(Adapt, ComponentView) {
+import Adapt from 'core/js/adapt';
+import ComponentView from 'core/js/views/componentView';
 
-  class AssessmentResultsView extends ComponentView {
+export default class AssessmentResultsView extends ComponentView {
 
-    events() {
-      return {
-        'click .js-assessment-retry-btn': 'onRetryClicked'
-      }
-    }
-
-    preRender() {
-      this.model.setLocking('_isVisible', false);
-
-      this.listenTo(Adapt.parentView, 'preRemove', function () {
-        this.model.unsetLocking('_isVisible');
-      });
-
-      this.listenTo(this.model, {
-        'change:_feedbackBand': this.addClassesToArticle,
-        'change:body': this.render
-      });
-    }
-
-    postRender() {
-      this.model.checkIfAssessmentComplete();
-      this.setReadyStatus();
-      this.setupInviewCompletion('.component__inner', this.model.checkCompletion.bind(this.model));
-    }
-
-    /**
-     * Resets the state of the assessment and optionally redirects the user
-     * back to the assessment for another attempt.
-     */
-    onRetryClicked() {
-      const state = this.model.get('_state');
-
-      Adapt.assessment.get(state.id).reset(null, wasReset => {
-        if (!wasReset) {
-          return;
-        }
-        if (this.model.get('_retry')._routeToAssessment === true) {
-          Adapt.navigateToElement('.' + state.articleId);
-        }
-      });
-    }
-
-    /**
-     * If there are classes specified for the feedback band, apply them to the containing article
-     * This allows for custom styling based on the band the user's score falls into
-     */
-    addClassesToArticle(model, value) {
-      if (!value || !value._classes) return;
-
-      this.$el.parents('.article').addClass(value._classes);
-    }
-
+  get template() {
+    return 'assessmentResults';
   }
 
-  AssessmentResultsView.template = 'assessmentResults';
+  events() {
+    return {
+      'click .js-assessment-retry-btn': 'onRetryClicked'
+    };
+  }
 
-  return AssessmentResultsView;
+  preRender() {
+    this.model.setLocking('_isVisible', false);
 
-});
+    this.listenTo(Adapt.parentView, 'preRemove', function () {
+      this.model.unsetLocking('_isVisible');
+    });
+
+    this.listenTo(this.model, {
+      'change:_feedbackBand': this.addClassesToArticle,
+      'change:body': this.render
+    });
+  }
+
+  postRender() {
+    this.model.checkIfAssessmentComplete();
+    this.setReadyStatus();
+    this.setupInviewCompletion('.component__inner', this.model.checkCompletion.bind(this.model));
+  }
+
+  /**
+   * Resets the state of the assessment and optionally redirects the user
+   * back to the assessment for another attempt.
+   */
+  onRetryClicked() {
+    const state = this.model.get('_state');
+
+    Adapt.assessment.get(state.id).reset(null, wasReset => {
+      if (!wasReset) return;
+
+      if (this.model.get('_retry')._routeToAssessment === true) {
+        Adapt.navigateToElement('.' + state.articleId);
+      }
+    });
+  }
+
+  /**
+   * If there are classes specified for the feedback band, apply them to the containing article
+   * This allows for custom styling based on the band the user's score falls into
+   */
+  addClassesToArticle(model, value) {
+    if (!value || !value._classes) return;
+
+    this.$el.parents('.article').addClass(value._classes);
+  }
+
+}
